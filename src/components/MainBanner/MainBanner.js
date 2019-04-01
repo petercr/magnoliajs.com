3286b4a2d6e93deeb3c34eb2a2c5863424e4a889
Peter <petecruckshank@gmail.com>
2019-04-01 12:19:54 -0400

    copied code from the master branch

--- src/components/MainBanner/MainBanner.js
+++ src/components/MainBanner/MainBanner.js
@@ -11,36 +11,32 @@ const Image = styled.img`
 `;
 
 function MainBanner({ children }) {
-  return (
-    <Flex
-      flexDirection={["column", "row-reverse"]}
-      my={[48, 0]}
-      alignItems="center"
-    >
-      <Box
-        width={[1, 1 / 2]}
-        alignSelf={["flex-start", "center"]}
-        justifyContent={["flex-start", "center"]}
-        px={30}
-      >
- 
+  return <Flex flexDirection={["column", "row-reverse"]} my={[48, 0]} alignItems="center">
+      <Box width={[1, 1 / 2]} alignSelf={["flex-start", "center"]} justifyContent={["flex-start", "center"]} px={30}>
+        <Flex justifyContent="center" flexDirection="column">
+          <Heading size={1} color="light" my={0} fontSize={6}>
+            MagnoliaJS Conference
+          </Heading>
+          <Text color="highlight" my={30}>
+            Jackson, Mississippi | April 17 - April 18
+          </Text>
+
           <Flex justifyContent="center" flexDirection="column">
             <Text color="white" my={30}>
-              Early Bird tickets on sale now!
-              <br />
-              <a
-                href="https://ti.to/magnoliajs/magnoliajs-2019"
-                title="Click here to purchase tickets to MagnoliaJS"
-                style={{ color: "yellow" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              Early Bird tickets on sale now!<br />
+              <a href="https://ti.to/magnoliajs/magnoliajs-2019" title="Click here to purchase tickets to MagnoliaJS" style={{ color: "yellow" }} target="_blank" rel="noopener noreferrer">
                 Purchase your ticket!
               </a>
             </Text>
           </Flex>
-        
-  );
+
+          {children}
+        </Flex>
+      </Box>
+      <Box width={[1, 1 / 2]}>
+        <Image src={bannerImg} alt="" />
+      </Box>
+    </Flex>;
 }
 
 export default MainBanner;
